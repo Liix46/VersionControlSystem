7ad06835ef77c8e99e86040348e44fac3cd3db4e
alxb46 <alexboichev46@gmail.com>
2022-07-21 14:07:01 +0300

    override toString method

--- VersionControlSystem/User.cs
+++ VersionControlSystem/User.cs
@@ -3,8 +3,11 @@ namespace VersionControlSystem
 {
     public class User
     {
-        public User()
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public override string ToString()
         {
+            return $"Name:{Name}  Age: {Age}";
         }
     }
 }

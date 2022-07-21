50aa273c53a9ee3e914c5423f8ab3af21ec38d24
alxb46 <alexboichev46@gmail.com>
2022-07-21 14:08:07 +0300

    Test class User

--- VersionControlSystem/Program.cs
+++ VersionControlSystem/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using VersionControlSystem;
+
 Console.WriteLine("Hello, World!");
+
+User user = new();
+user.Name = "Tom HOlland";
+user.Age = 23;
+
+Console.WriteLine(user.ToString());
 

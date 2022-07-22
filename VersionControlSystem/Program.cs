66e4700ef6155510fe0ab6e37b158c553ddb5d71
alxb46 <alexboichev46@gmail.com>
2022-07-22 13:16:37 +0300

    delete to string

--- VersionControlSystem/Program.cs
+++ VersionControlSystem/Program.cs
@@ -3,12 +3,3 @@
 User user = new();
 user.Name = "Tom Holland";
 user.Age = 23;
-
-Console.WriteLine(user.ToString());
-
-
-<<<<<<< HEAD
-=======
-user.Name = "first name";
-user.Name = "last name";
->>>>>>> main

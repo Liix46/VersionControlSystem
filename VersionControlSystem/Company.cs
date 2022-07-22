c24b214f8f383f3f6f6551ed8898a0e4ba4ab390
alxb46 <alexboichev46@gmail.com>
2022-07-22 14:23:32 +0300

    add class Company

---
+++ VersionControlSystem/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace VersionControlSystem
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public Company(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
+
--- VersionControlSystem/Program.cs
+++ VersionControlSystem/Program.cs
@@ -2,11 +2,16 @@
 
 User user = new();
 user.Name = "Tom Holland";
-user.Age = 23;
+
 
 user.Sex = "Male";
 user.RememberMe = true;
 
+user.Age = 25;
+
 Console.WriteLine(user.ToString());
 
+Company company = new("Vision");
+
+Console.WriteLine(company.ToString());
 

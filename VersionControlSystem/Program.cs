a4659bb5e3eea5078140c9c931ab573fa8da95f0
alxb46 <alexboichev46@gmail.com>
2022-07-22 13:09:58 +0300

    delete strings

--- VersionControlSystem/Program.cs
+++ VersionControlSystem/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using VersionControlSystem;
-
-Console.WriteLine("Hello, World!");
+﻿using VersionControlSystem;
 
 User user = new();
 user.Name = "Tom Holland";
@@ -9,8 +6,4 @@ user.Age = 23;
 
 Console.WriteLine(user.ToString());
 
-user.Age += 1;
-Console.WriteLine(user.ToString());
 
-user.Name = "name";
-user.Name = "lastname";
